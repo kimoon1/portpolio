39740bcc65b86c7711b07a40aca54e8c81d2faf6
kimoon <kimoon212@gmail.com>
2024-10-23 12:58:26 +0900

    init

---
+++ app/(home)/page.tsx
@@ -0,0 +1,17 @@
+
+export const metadata = {
+  title: 'Home',
+
+}
+
+
+
+
+
+
+export default async function HomePage() {
+  
+  return <main>
+    <h1>home</h1>
+  </main>
+}
---
+++ components/HeaderComponent.tsx
@@ -0,0 +1,13 @@
+export default function HeaderComponent() {
+  return (
+    <header>
+      <ul>
+        <li>About</li>
+        <li>Articles</li>
+        <li>Projects</li>
+        <li>Speaking</li>
+        <li>Uses</li>
+      </ul>
+    </header>
+  );
+}
