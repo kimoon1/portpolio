6f218b592793745df48d4144bb06039b71ce8e77
kimoon <kimoon212@gmail.com>
2024-10-31 17:36:06 +0900

    [css]header ui update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,12 @@ import styles from './Header.module.scss';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>My Website</h1>
+      <ul className={styles.menu}>
+        <li className={`${styles['menu-item']}`}>Home</li>
+        <li className={`${styles['menu-item']}`}>Portfolio</li>
+        <li className={`${styles['menu-item']}`}>Info</li>
+        <li className={`${styles['menu-item']}`}>Etc</li>
+      </ul>
     </header>
   );
 }
