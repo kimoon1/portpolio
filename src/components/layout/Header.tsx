8e2f5c9abd06e29424df9111ebbf503c7db3f321
kimoon <kimoon212@gmail.com>
2024-11-01 13:18:12 +0900

    [css] 헤더 스타일 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link';
 import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <ul className={styles.menu}>
-        <li className={`${styles['menu-item']}`}>Home</li>
-        <li className={`${styles['menu-item']}`}>Portfolio</li>
-        <li className={`${styles['menu-item']}`}>Info</li>
-        <li className={`${styles['menu-item']}`}>Etc</li>
+        <li className={`${styles['menu-item']}`}><Link href={'/'}>Home</Link></li>
+        <li className={`${styles['menu-item']}`}><Link href={'/'}>Portfolio</Link></li>
+        <li className={`${styles['menu-item']}`}><Link href={'/'}>Info</Link></li>
+        <li className={`${styles['menu-item']}`}><Link href={'/'}>Etc</Link></li>
       </ul>
     </header>
   );
