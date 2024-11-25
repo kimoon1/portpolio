d2c43f75edade5fae75fdf59a63ef525ea2513d0
kimoon <kimoon212@gmail.com>
2024-11-25 15:18:22 +0900

    메인 작업중 - swiper 모듈 및 테스트 이미지 설정

--- src/components/home/ProjectThumb.tsx
+++ src/components/home/ProjectThumb.tsx
@@ -2,22 +2,56 @@
 'use client';
 
 import React from 'react';
+
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import styles from '@/src/styles/main/projectthumb.module.scss'
+import Image from 'next/image';
 
 const SwiperComponent = () => {
-  return (
+  return (<>
     <Swiper
-      speed={400}
-      spaceBetween={100}
+      wrapperClass={`swiper-wrapper ${styles['swiper-wrapper']}`}
+      modules={[Navigation, Pagination]}
+      navigation={{
+        enabled: true,
+        nextEl: `.${styles['swiper-button-next']}`,
+        prevEl: `.${styles['swiper-button-prev']}`,
+        disabledClass: `${styles['swiper-button-disabled']}`
+      }}
+      pagination={{ 
+        enabled: true,
+        clickable: true,
+       }}
+      speed={500}
+      spaceBetween={0}
       className={`${styles['swiper']}`}
+      
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>
+        <div className={`${styles['img-wrap']}`}>
+
+          <Image width={1536} height={1536} alt='test' src={'/home/ProjectThumb/test01.jpg'} />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className={`${styles['img-warp']}`}>
+          <Image width={1536} height={1536} alt='test' src={'/home/ProjectThumb/test02.jpg'} />
+        </div>
+      </SwiperSlide>
+      <div className={`${styles['swiper-button-next']}`}>
+        <Image width={40} height={40} alt='화살표' src={'/home/ProjectThumb/rightArrow.svg'} />
+      </div>
+      <div className={`${styles['swiper-button-prev']}`}>
+        <Image width={40} height={40} alt='화살표' src={'/home/ProjectThumb/leftArrow.svg'} />
+      </div>
     </Swiper>
+  </>
   );
 };
 
